142569f8f040a8415c7ea0b7a0d5f10f848f6cd8
redPanda <y1433011488@outlook.com>
2019-12-08 17:36:11 +0800

    fixing course_select

--- src/lib/servlet/course_addServlet.java
+++ src/lib/servlet/course_addServlet.java
@@ -37,7 +37,22 @@ public class course_addServlet extends HttpServlet{
         course.setCourse_name(map.get("name"));
         course.setAttribute(map.get("attribute"));
         course.setDuration(map.get("duration"));
-        course.setWeekday(map.get("days"));
+        switch (map.get("days")){
+            case "0":course.setWeekday("星期日");
+            break;
+            case "1":course.setWeekday("星期一");
+                break;
+            case "2":course.setWeekday("星期二");
+                break;
+            case "3":course.setWeekday("星期三");
+                break;
+            case "4":course.setWeekday("星期四");
+                break;
+            case "5":course.setWeekday("星期五");
+                break;
+            case "6":course.setWeekday("星期六");
+                break;
+        }
         course.setDescription(map.get("description"));
         course.setRefer(map.get("refer"));
         course.setWeek_start(map.get("week_start"));
